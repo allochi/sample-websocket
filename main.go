7a542e33f311edddc7c90f3ca4cdc501121319b2
allochi <allochi@gmail.com>
2024-02-19 11:26:06 +0100

    working example

--- main.go
+++ main.go
@@ -1,78 +1,126 @@
 package main
 
 import (
-	"net"
+	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/gobwas/ws"
-	"github.com/gobwas/ws/wsutil"
-	"github.com/google/uuid"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
-type User struct {
-	ID   string
-	Name string
-	Conn *net.Conn
+// address => ws_connection -> last_active = we can clean up inactive connections after some time
+
+// TODO
+// 1. maybe we can add the address to the user of notification
+// 2. maybe we add a table of notifications that we can sen to the client when they connect
+
+type WSConn struct {
+	*websocket.Conn
+	LastActive time.Time
 }
+
+// var connections = make(map[string]*websocket.Conn)
+var connections = make(map[string]*WSConn)
 
 type Message struct {
-	From    string
-	To      string
-	Content string
+	Address string
+	Msg     []byte
 }
+
+var messages []Message
 
 func main() {
-	users := make(map[string]User)
 
-	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// upgrade connection
-		conn, _, _, err := ws.UpgradeHTTP(r, w)
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			// Replace with your origin validation logic
+			return true // Allow all origins for demonstration
+		},
+	}
+
+	g := gin.Default()
+
+	g.Static("/public", "./public")
+
+	g.POST("/send", func(c *gin.Context) {
+		var payload struct {
+			Address string `json:"address"`
+		}
+		c.BindJSON(&payload)
+		message := Message{payload.Address, []byte(fmt.Sprintf("Transaction done for: %s", payload.Address))}
+		messages = append(messages, message)
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	g.GET("/notifications", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 		if err != nil {
-			// handle error
+			fmt.Println("Upgrade:", err)
+			return
 		}
 
-		// track connection
-		id := uuid.New().String()
-		users[id] = User{
-			ID:   id,
-			Conn: &conn,
+		fmt.Println("Client connected:", conn.RemoteAddr())
+
+		// Read address and store connection
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Read:", err)
 		}
 
-		// handle requests
-		// warn: new goroutine with every request?
-		go func() {
-			// close connection on shutdown
-			defer conn.Close()
+		connections[string(message)] = &WSConn{conn, time.Now()}
+	})
 
-			// ...
-			for {
-				msg, op, err := wsutil.ReadClientData(conn)
-				if err != nil {
-					// handle error
-				}
+	// var previousNumGoroutine int
+	// go func() {
+	// 	for {
+	// 		<-time.After(1 * time.Second)
+	// 		currentNumGoroutine := runtime.NumGoroutine()
+	// 		if currentNumGoroutine != previousNumGoroutine {
+	// 			fmt.Println("NumGoroutine: ", currentNumGoroutine)
+	// 			previousNumGoroutine = currentNumGoroutine
+	// 		}
+	// 	}
+	// }()
 
-				// ...
-				for _, user := range users {
-					// if id == user.ID {
-					// 	continue
-					// }
-					// log.Printf("send to %s", id)
-					// msg = []byte(fmt.Sprintf("%s: %s", id, string(msg)))
-					err := wsutil.WriteServerMessage(*user.Conn, op, msg)
-					if err != nil {
-						// handle error
-					}
-				}
-
-				// err = wsutil.WriteServerMessage(conn, op, msg)
-				// if err != nil {
-				// 	// handle error
-				// }
+	// consume messages and send to client
+	go func() {
+		for {
+			<-time.After(1 * time.Second)
+			if len(messages) > 0 {
+				msg := messages[0]
+				messages = messages[1:]
+				send(msg.Address, msg.Msg)
 			}
-		}()
-	}))
+		}
+	}()
+
+	// remove inactive connections
+	go func() {
+		for {
+			for address, conn := range connections {
+				if time.Since(conn.LastActive) > 15*time.Second {
+					fmt.Println("Client disconnected:", conn.RemoteAddr())
+					conn.Close()
+					delete(connections, address)
+				}
+			}
+		}
+	}()
+
+	g.Run(":3000")
 }
 
-func UnmarshalMessage(msg []byte) (Message, error) {
-	return Message{}, nil
+func send(address string, msg []byte) {
+	conn, ok := connections[address]
+	if ok {
+		err := conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			fmt.Println("Write:", err)
+			delete(connections, address)
+		}
+		conn.LastActive = time.Now()
+	}
 }
